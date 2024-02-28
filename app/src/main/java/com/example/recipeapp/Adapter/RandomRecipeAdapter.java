60bc7c1e50767a749059412bff767b8734b66558
Balakrishna071001 <balakrishna.bhubarpa@gmail.com>
2024-02-28 23:26:39 +0530

    day third

---
+++ app/src/main/java/com/example/recipeapp/Adapter/RandomRecipeAdapter.java
@@ -0,0 +1,67 @@
+package com.example.recipeapp.Adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.recipeapp.Models.Recipe;
+import com.example.recipeapp.R;
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+public class RandomRecipeAdapter extends RecyclerView.Adapter<RandomRecipeViewHolder> {
+    Context  context;
+    List<Recipe> list;
+
+    public RandomRecipeAdapter(Context context, List<Recipe> list) {
+        this.context = context;
+        this.list = list;
+    }
+
+    @NonNull
+    @Override
+    public RandomRecipeViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new RandomRecipeViewHolder(LayoutInflater.from(context).inflate(R.layout.list_random_recipe, parent, false));
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RandomRecipeViewHolder holder, int position) {
+        holder.textView_title.setText(list.get(position).title);
+        holder.textView_title.setSelected(true);
+        holder.textView_likes.setText(list.get(position).aggregateLikes+" Likes ");
+        holder.textView_servings.setText(list.get(position).servings+" Servings ");
+        holder.textView_time.setText(list.get(position).readyInMinutes+" Minutes ");
+        Picasso.get().load(list.get(position).image).into(holder.imageView_food);
+    }
+
+    @Override
+    public int getItemCount() {
+        return list.size();
+    }
+}
+class RandomRecipeViewHolder extends RecyclerView.ViewHolder {
+    CardView random_list_container;
+    TextView textView_title,textView_servings,textView_likes, textView_time;
+    ImageView imageView_food;
+    public RandomRecipeViewHolder(@NonNull View itemView) {
+        super(itemView);
+        random_list_container = itemView.findViewById(R.id.random_list_container);
+        textView_title = itemView.findViewById(R.id.textView_title);
+        textView_servings = itemView.findViewById(R.id.textView_servings);
+        textView_likes = itemView.findViewById(R.id.textView_likes);
+        textView_time = itemView.findViewById(R.id.textView_time);
+        imageView_food = itemView.findViewById(R.id.imageView_food);
+
+
+
+    }
+}
